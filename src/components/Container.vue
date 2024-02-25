95f4a9881e50c29a3462efa59c024e3b5c562c18
em1128 <poseidon_x@naver.com>
2024-02-25 18:57:08 +0900

    first commit

---
+++ src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-for="(a, i) in insta" :key="i">
+    <Post :insta="a"></Post>
+  </div>
+
+</template>
+
+<script>
+import Post from './Post.vue'
+
+export default {
+    
+    components : {
+        Post,
+    },
+    props : {
+        insta : Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
